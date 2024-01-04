91af7143257069483c0512fb77534c7ddb67eb38
Kushal-Kamble <^[[200~professionalkamble199@gmail.com~>
2024-01-04 16:48:42 +0530

    My tenh commit on git hub

---
+++ src/11-app.js
@@ -0,0 +1,49 @@
+import "./App.css";
+
+import {Header} from './components/Header'; // Application khuch bhi name rakh sakte hai
+
+import Student from './components/Student';
+
+function App() {
+  let name =  "Kushal";
+  return (
+
+    
+
+    
+    <div className="container kushal">
+      <div className="row ">  
+
+        <Header/>
+
+      <h1>{name}</h1>
+
+      <Student name="kushal" age={25} />
+      <Headerr/>
+
+      {/* <Student name="vishal" age={30} />
+
+      <Student name="salman" age={31} />
+
+      <Student name="timepass" age={10} /> */}
+
+      <h1>my seven commit</h1>
+      <h1>my eight commit</h1>
+
+       
+      
+        
+       
+      </div>
+    </div>
+
+
+
+
+    
+      
+    
+  );
+}
+
+export default App;
